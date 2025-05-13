1df85cbb8d25a102523c847fb4382afaeb223604
bahagiadamas <176278780+bahagiadamas@users.noreply.github.com>
2025-05-13 22:20:01 +0700

    NEW DEV

--- public/js/modules/database.js
+++ public/js/modules/database.js
@@ -1,6 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js";
-import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-analytics.js";
+import {
+  getAnalytics,
+  logEvent,
+} from "https://www.gstatic.com/firebasejs/11.7.1/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCHNhYJJZuy1_1kPhh2dGQSC46gIrINYlk",
@@ -14,4 +18,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
-export { db, analytics, logEvent };
+const auth = getAuth(app);
+export { db, analytics, logEvent, auth };
